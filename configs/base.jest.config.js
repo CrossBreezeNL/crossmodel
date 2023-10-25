e04604b72e58b99644952c25204c5e25b9ac81c4
Harmen Wessels <97173058+harmen-xb@users.noreply.github.com>
2023-10-26 00:53:45 +0200

    Added skipping node_modules in jest base config.

--- configs/base.jest.config.js
+++ configs/base.jest.config.js
@@ -2,5 +2,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    testPathIgnorePatterns: ['./node_modules/'],
     reporters: ['default', ['jest-junit', { outputDirectory: 'unit-test-results', outputName: 'jest-report.xml' }]]
 };

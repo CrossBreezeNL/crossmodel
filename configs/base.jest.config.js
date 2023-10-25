11f4f2f65e0a343e55d6ab161cd51555124f371c
Harmen Wessels <97173058+harmen-xb@users.noreply.github.com>
2023-10-26 00:27:35 +0200

    Updated jest config to work for alle applications, extensions and packages.

---
+++ configs/base.jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    reporters: ['default', ['jest-junit', { outputDirectory: 'unit-test-results', outputName: 'jest-report.xml' }]]
+};

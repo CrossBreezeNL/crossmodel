ab2a8fbb459ea2921e5154af4332a3867babd65e
Harmen Wessels <97173058+harmen-xb@users.noreply.github.com>
2023-10-27 10:51:18 +0200

    Added jest to tslint configuration Refactored tsconfig to all extend the base tsconfig and have specific one in the root of the workspace.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
     root: true,
-    extends: [
-        './configs/base.eslintrc.js',
-        './configs/warnings.eslintrc.js',
-        './configs/errors.eslintrc.js'
-    ],
-    ignorePatterns: ['**/{node_modules,lib}', '**/.eslintrc.js', 'extensions/**/generated'],
+    extends: ['./configs/base.eslintrc.js', './configs/warnings.eslintrc.js', './configs/errors.eslintrc.js'],
+    ignorePatterns: ['**/{node_modules,lib}', 'plugins', '**/.eslintrc.js', 'extensions/**/generated'],
     parserOptions: {
         tsconfigRootDir: __dirname,
         project: 'tsconfig.eslint.json'
     },
     settings: {
-       react: {
-          version: 'detect'
-       }
+        react: {
+            version: 'detect'
+        }
     }
 };

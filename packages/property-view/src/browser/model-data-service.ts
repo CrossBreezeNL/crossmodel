a51dce354c92e0aa6a6db5672f025908b6290b04
Martin Fleck <mfleck@eclipsesource.com>
2023-12-01 13:21:24 +0100

    Fix "Property widget starts flashing"
    Problem: We open an entity through the property view. That triggers a
    rebuild of the entity document which triggers an update in all open
    diagrams that have that entity as they have a reference to it. The
    diagram update also resets the selection state in each diagram which
    in turn updates the property view again as it reacts to selection
    changes. This can lead to an update-cycle if there is more than one
    diagram open with the same entity.
    
    Fix: Since the property view reacts to selection changes and updates in
    the diagram trigger such a change, there is no need for the property
    view to listen actually participate in the model lifecycle itself. So
    there is no need to do the open/close since we do not need to listen to
    updates ourselves.

--- packages/property-view/src/browser/model-data-service.ts
+++ packages/property-view/src/browser/model-data-service.ts
@@ -4,7 +4,7 @@
 
 import { ENTITY_NODE_TYPE } from '@crossbreeze/glsp-client/lib/browser/model';
 import { ModelService } from '@crossbreeze/model-service/lib/common';
-import { CrossModelRoot, DiagramNodeEntity } from '@crossbreeze/protocol';
+import { DiagramNodeEntity } from '@crossbreeze/protocol';
 import { GlspSelection } from '@eclipse-glsp/theia-integration';
 import { inject, injectable } from '@theia/core/shared/inversify';
 import { PropertyDataService } from '@theia/property-view/lib/browser/property-data-service';
@@ -15,33 +15,11 @@ export const PROPERTY_CLIENT_ID = 'property-view-client';
 export class ModelDataService implements PropertyDataService {
    id = 'model-property-data-service';
    label = 'ModelPropertyDataService';
-   currentUri?: string;
 
    @inject(ModelService) protected modelService: ModelService;
 
    canHandleSelection(selection: GlspSelection | undefined): number {
-      const canHandle = GlspSelection.is(selection) ? 1 : 0;
-
-      // Close the previous file if there is a new selection the property view can not handle
-      if (canHandle === 0 && this.currentUri) {
-         this.modelService.close({ uri: this.currentUri, clientId: PROPERTY_CLIENT_ID });
-      }
-
-      return canHandle;
-   }
-
-   protected async closeCurrentModel(): Promise<void> {
-      if (this.currentUri) {
-         return this.modelService.close({ uri: this.currentUri, clientId: PROPERTY_CLIENT_ID });
-      }
-      this.currentUri = undefined;
-   }
-
-   protected async openCurrentModel(): Promise<CrossModelRoot | undefined> {
-      if (this.currentUri) {
-         return this.modelService.open({ uri: this.currentUri, clientId: PROPERTY_CLIENT_ID });
-      }
-      return undefined;
+      return GlspSelection.is(selection) ? 1 : 0;
    }
 
    protected async getSelectedEntity(selection: GlspSelection | undefined): Promise<DiagramNodeEntity | undefined> {
@@ -57,17 +35,6 @@ export class ModelDataService implements PropertyDataService {
    }
 
    async providePropertyData(selection: GlspSelection | undefined): Promise<DiagramNodeEntity | undefined> {
-      const entity = await this.getSelectedEntity(selection);
-      if (!entity) {
-         this.closeCurrentModel();
-         return undefined;
-      }
-      const newUri = entity.uri;
-      if (newUri !== this.currentUri) {
-         await this.closeCurrentModel();
-      }
-      this.currentUri = newUri;
-      await this.openCurrentModel();
-      return entity;
+      return this.getSelectedEntity(selection);
    }
 }

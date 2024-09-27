c0075c70065569a80c9944b23cde37eb6085ba42
Martin Fleck <mfleck@eclipsesource.com>
2024-09-27 17:00:54 +0200

    Remove duplicate entries for auto-completion suggestions and sort
    - Prefer package local names over global names
    - Sort local names before global name in textual and diagram editor
    
    Fixes https://github.com/CrossBreezeNL/crossmodel/issues/66

--- extensions/crossmodel-lang/test/language-server/test-utils/utils.ts
+++ extensions/crossmodel-lang/test/language-server/test-utils/utils.ts
@@ -2,9 +2,11 @@
  * Copyright (c) 2023 CrossBreeze.
  ********************************************************************************/
 
-import { EmptyFileSystem, LangiumDocument } from 'langium';
-import { LangiumServices } from 'langium/lsp';
+import { EmptyFileSystem, FileSystemNode, FileSystemProvider, LangiumDocument, URI } from 'langium';
+import { DefaultSharedModuleContext, LangiumServices } from 'langium/lsp';
 import { ParseHelperOptions, parseDocument as langiumParseDocument } from 'langium/test';
+import path from 'path';
+import { PackageJson } from 'type-fest';
 import { CrossModelServices, createCrossModelServices } from '../../../src/language-server/cross-model-module.js';
 import {
    CrossModelRoot,
@@ -19,15 +21,26 @@ import {
 } from '../../../src/language-server/generated/ast.js';
 import { SemanticRoot, TypeGuard, WithDocument, findSemanticRoot } from '../../../src/language-server/util/ast-util.js';
 
-export function createCrossModelTestServices(): CrossModelServices {
-   return createCrossModelServices({ ...EmptyFileSystem }).CrossModel;
+export function createCrossModelTestServices(context: DefaultSharedModuleContext = EmptyFileSystem): CrossModelServices {
+   return createCrossModelServices(context).CrossModel;
 }
 
-export const parseDocument = langiumParseDocument<CrossModelRoot>;
+export interface ProjectInput {
+   package: PackageInput;
+   documents: DocumentInput[];
+}
 
-export interface ParseInput<T = string | string[]> extends ParseHelperOptions {
+export interface PackageInput {
    services: LangiumServices;
-   text: T;
+   uri: string;
+   content: PackageJson & { name: string; version: string };
+}
+
+export type PackageDependency = Record<string, string>;
+
+export interface DocumentInput extends ParseHelperOptions {
+   services: LangiumServices;
+   text: string;
 }
 
 export interface ParseAssert {
@@ -35,20 +48,37 @@ export interface ParseAssert {
    parserErrors?: number;
 }
 
-export async function parseDocuments(
-   services: LangiumServices,
-   inputs: string[],
-   options?: ParseHelperOptions
-): Promise<LangiumDocument<CrossModelRoot>[]> {
-   return Promise.all(inputs.map(input => parseDocument(services, input, options)));
+export async function parseProject(input: ProjectInput): Promise<PackageDependency> {
+   const projectDependency = await parsePackage(input.package);
+   await parseDocuments(input.documents);
+   return projectDependency;
+}
+
+export async function parsePackage(input: PackageInput): Promise<PackageDependency> {
+   await parseDocument({ text: JSON.stringify(input.content), documentUri: input.uri, services: input.services });
+   return { [input.content.name]: input.content.version };
+}
+
+export async function parseDocument(input: DocumentInput): Promise<LangiumDocument<CrossModelRoot>> {
+   if (input.documentUri) {
+      const fileSystemProvider = input.services.shared.workspace.FileSystemProvider;
+      if (fileSystemProvider instanceof MockFileSystemProvider) {
+         fileSystemProvider.setFile(URI.parse(input.documentUri), input.text);
+      }
+   }
+   return langiumParseDocument<CrossModelRoot>(input.services, input.text, input);
+}
+
+export async function parseDocuments(inputs: DocumentInput[]): Promise<LangiumDocument<CrossModelRoot>[]> {
+   return Promise.all(inputs.map(parseDocument));
 }
 
 export async function parseSemanticRoot<T extends SemanticRoot>(
-   input: ParseInput<string>,
+   input: DocumentInput,
    assert: ParseAssert,
    guard: TypeGuard<T>
 ): Promise<WithDocument<T>> {
-   const document = await parseDocument(input.services, input.text, input);
+   const document = await parseDocument(input);
    expect(document.parseResult.lexerErrors).toHaveLength(assert.lexerErrors ?? 0);
    expect(document.parseResult.parserErrors).toHaveLength(assert.parserErrors ?? 0);
    const semanticRoot = findSemanticRoot(document, guard);
@@ -57,18 +87,43 @@ export async function parseSemanticRoot<T extends SemanticRoot>(
    return semanticRoot as WithDocument<T>;
 }
 
-export async function parseEntity(input: ParseInput<string>, assert: ParseAssert = {}): Promise<WithDocument<Entity>> {
+export async function parseEntity(input: DocumentInput, assert: ParseAssert = {}): Promise<WithDocument<Entity>> {
    return parseSemanticRoot(input, assert, isEntity);
 }
 
-export async function parseRelationship(input: ParseInput<string>, assert: ParseAssert = {}): Promise<WithDocument<Relationship>> {
+export async function parseRelationship(input: DocumentInput, assert: ParseAssert = {}): Promise<WithDocument<Relationship>> {
    return parseSemanticRoot(input, assert, isRelationship);
 }
 
-export async function parseSystemDiagram(input: ParseInput<string>, assert: ParseAssert = {}): Promise<WithDocument<SystemDiagram>> {
+export async function parseSystemDiagram(input: DocumentInput, assert: ParseAssert = {}): Promise<WithDocument<SystemDiagram>> {
    return parseSemanticRoot(input, assert, isSystemDiagram);
 }
 
-export async function parseMapping(input: ParseInput<string>, assert: ParseAssert = {}): Promise<WithDocument<Mapping>> {
+export async function parseMapping(input: DocumentInput, assert: ParseAssert = {}): Promise<WithDocument<Mapping>> {
    return parseSemanticRoot(input, assert, isMapping);
+}
+
+export const MockFileSystem: DefaultSharedModuleContext = {
+   fileSystemProvider: () => new MockFileSystemProvider()
+};
+
+export class MockFileSystemProvider implements FileSystemProvider {
+   protected fileContent = new Map<string, string>();
+
+   setFile(uri: URI, content: string): void {
+      this.fileContent.set(uri.toString(), content);
+   }
+
+   async readFile(uri: URI): Promise<string> {
+      return this.fileContent.get(uri.toString()) ?? '';
+   }
+
+   async readDirectory(uri: URI): Promise<FileSystemNode[]> {
+      return [];
+   }
+}
+
+export function testUri(...segments: string[]): string {
+   // making sure the URI works on both Windows and Unix
+   return 'test:///' + path.join(...segments);
 }

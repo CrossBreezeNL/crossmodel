b5112b7a0f2d59d24f0c0dfde4aca84932c82b0f
Martin Fleck <mfleck@eclipsesource.com>
2024-12-20 14:33:41 +0000

    Setup clean default layout for application (#79)
    * Setup clean default layout for application
    
    * Add Git and Open VSX registry ('Extension') support

---
+++ packages/product/src/browser/cross-model-frontend-application.ts
@@ -0,0 +1,33 @@
+/********************************************************************************
+ * Copyright (c) 2024 CrossBreeze.
+ ********************************************************************************/
+import { ApplicationShell, FrontendApplication, WidgetManager } from '@theia/core/lib/browser';
+import { inject } from '@theia/core/shared/inversify';
+import { PROBLEMS_WIDGET_ID } from '@theia/markers/lib/browser/problem/problem-widget';
+import { NavigatorWidgetFactory } from '@theia/navigator/lib/browser/navigator-widget-factory';
+import { PropertyViewWidget } from '@theia/property-view/lib/browser/property-view-widget';
+import { SCM_VIEW_CONTAINER_ID } from '@theia/scm/lib/browser/scm-contribution';
+import { SearchInWorkspaceWidget } from '@theia/search-in-workspace/lib/browser/search-in-workspace-widget';
+
+export class CrossModelFrontendApplication extends FrontendApplication {
+   @inject(WidgetManager) protected readonly widgetManager: WidgetManager;
+
+   protected override async createDefaultLayout(): Promise<void> {
+      // ignores all FrontendApplicationContribution.initializeLayout calls and focusses on a very clean interface
+      await this.showWidget(NavigatorWidgetFactory.ID, 'left');
+      await this.showWidget(SearchInWorkspaceWidget.ID, 'left', false);
+      await this.showWidget(SCM_VIEW_CONTAINER_ID, 'left', false);
+      await this.showWidget(PropertyViewWidget.ID, 'right');
+      await this.showWidget(PROBLEMS_WIDGET_ID, 'bottom');
+      this.shell.leftPanelHandler.resize(400);
+      this.shell.rightPanelHandler.resize(600);
+   }
+
+   protected async showWidget(widgetId: string, area: ApplicationShell.Area, reveal = true, rank = 100): Promise<void> {
+      const sdvTreeWidget = await this.widgetManager.getOrCreateWidget(widgetId);
+      this.shell.addWidget(sdvTreeWidget, { area, rank });
+      if (reveal) {
+         this.shell.revealWidget(widgetId);
+      }
+   }
+}
--- packages/product/src/browser/product-frontend-module.ts
+++ packages/product/src/browser/product-frontend-module.ts
@@ -1,11 +1,20 @@
 /********************************************************************************
  * Copyright (c) 2024 CrossBreeze.
  ********************************************************************************/
+import { ApplicationShell, ApplicationShellOptions, FrontendApplication } from '@theia/core/lib/browser';
 import { ContainerModule } from '@theia/core/shared/inversify';
 import { ThemeServiceWithDB } from '@theia/monaco/lib/browser/monaco-indexed-db';
+import { CrossModelFrontendApplication } from './cross-model-frontend-application';
 import { CrossModelThemeService } from './cross-model-theme-service';
 
 export default new ContainerModule((bind, _unbind, _isBound, rebind) => {
    bind(CrossModelThemeService).toSelf().inSingletonScope();
    rebind(ThemeServiceWithDB).toService(CrossModelThemeService);
+
+   rebind(FrontendApplication).to(CrossModelFrontendApplication).inSingletonScope();
+   rebind(ApplicationShellOptions).toConstantValue(<ApplicationShell.Options>{
+      bottomPanel: {
+         initialSizeRatio: 0.25 // default: 0.382
+      }
+   });
 });
